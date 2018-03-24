b35de9ce1038fef9cc6c62115781bc639bbf20dc
Yotam Salmon <yotam.salmon@gmail.com>
2018-03-24 14:45:21 +0300

    Add support for HTTPS

--- featured.php
+++ featured.php
@@ -2,26 +2,26 @@
 header("Access-Control-Allow-Origin: *");
 ?>
 [
-	{"name": "A321 (Etihad) by Alta Aviation", "id": 339, "url": "http://aircraft-loader.appspot.com/load.php?id=339"},
+	{"name": "A321 (Etihad) by Alta Aviation", "id": 339, "url": "https://aircraft-loader.appspot.com/load.php?id=339"},
 	
-    {"name": "CRJ-900 (Lufthansa) by Yotam", "id": 319, "url": "http://aircraft-loader.appspot.com/load.php?id=319"},
-	{"name": "Dash-8 Q400 (KSAir) by Yotam", "id": 320, "url": "http://aircraft-loader.appspot.com/load.php?id=320"},
-	{"name": "717 (Hawaiian) by DBAPY", "id": 301, "url": "http://aircraft-loader.appspot.com/load.php?id=301"},
+    {"name": "CRJ-900 (Lufthansa) by Yotam", "id": 319, "url": "https://aircraft-loader.appspot.com/load.php?id=319"},
+	{"name": "Dash-8 Q400 (KSAir) by Yotam", "id": 320, "url": "https://aircraft-loader.appspot.com/load.php?id=320"},
+	{"name": "717 (Hawaiian) by DBAPY", "id": 301, "url": "https://aircraft-loader.appspot.com/load.php?id=301"},
 	
-	{"name": "737-800 (Ryanair) by Yotam", "id": 338, "url": "http://aircraft-loader.appspot.com/load.php?id=338"},
-	{"name": "737-800 (American) by Yotam", "id": 321, "url": "http://aircraft-loader.appspot.com/load.php?id=321"},
-	{"name": "737-800 (KLM) by Yotam", "id": 322, "url": "http://aircraft-loader.appspot.com/load.php?id=322"},
-	{"name": "737-800 (British Airways) [★Event★] by Yotam", "id": 323, "url": "http://aircraft-loader.appspot.com/load.php?id=323"},
-	{"name": "737-800 (United) by Yotam", "id": 324, "url": "http://aircraft-loader.appspot.com/load.php?id=324"},
-	{"name": "737-800 (Delta) by Yotam", "id": 325, "url": "http://aircraft-loader.appspot.com/load.php?id=325"},
-	{"name": "737-800 (American old) by Yotam", "id": 326, "url": "http://aircraft-loader.appspot.com/load.php?id=326"},
+	{"name": "737-800 (Ryanair) by Yotam", "id": 338, "url": "https://aircraft-loader.appspot.com/load.php?id=338"},
+	{"name": "737-800 (American) by Yotam", "id": 321, "url": "https://aircraft-loader.appspot.com/load.php?id=321"},
+	{"name": "737-800 (KLM) by Yotam", "id": 322, "url": "https://aircraft-loader.appspot.com/load.php?id=322"},
+	{"name": "737-800 (British Airways) [★Event★] by Yotam", "id": 323, "url": "https://aircraft-loader.appspot.com/load.php?id=323"},
+	{"name": "737-800 (United) by Yotam", "id": 324, "url": "https://aircraft-loader.appspot.com/load.php?id=324"},
+	{"name": "737-800 (Delta) by Yotam", "id": 325, "url": "https://aircraft-loader.appspot.com/load.php?id=325"},
+	{"name": "737-800 (American old) by Yotam", "id": 326, "url": "https://aircraft-loader.appspot.com/load.php?id=326"},
 	
-	{"name": "777-200 (ELAL) by Anonymous Contributor", "id": 334, "url": "http://aircraft-loader.appspot.com/load.php?id=334"},
-	{"name": "777-200 (Alitalia) [★New★]", "id": 335, "url": "http://aircraft-loader.appspot.com/load.php?id=335"},
-	{"name": "777-200 (ANA) [★New★]", "id": 336, "url": "http://aircraft-loader.appspot.com/load.php?id=336"},
+	{"name": "777-200 (ELAL) by Anonymous Contributor", "id": 334, "url": "https://aircraft-loader.appspot.com/load.php?id=334"},
+	{"name": "777-200 (Alitalia) [★New★]", "id": 335, "url": "https://aircraft-loader.appspot.com/load.php?id=335"},
+	{"name": "777-200 (ANA) [★New★]", "id": 336, "url": "https://aircraft-loader.appspot.com/load.php?id=336"},
 	
-	{"name": "A380 (Emirates) [derived from Xavier's A380]", "id": 328, "url": "http://aircraft-loader.appspot.com/load.php?id=328"},
-	{"name": "A380 (Korean) [derived from Xavier's A380]", "id": 329, "url": "http://aircraft-loader.appspot.com/load.php?id=329"},
-	{"name": "A380 (British Airways) [derived from Xavier's A380]", "id": 330, "url": "http://aircraft-loader.appspot.com/load.php?id=330"},
-	{"name": "A380 (Dubai One) [Livery by Siddharth]", "id": 333, "url": "http://aircraft-loader.appspot.com/load.php?id=333"}
+	{"name": "A380 (Emirates) [derived from Xavier's A380]", "id": 328, "url": "https://aircraft-loader.appspot.com/load.php?id=328"},
+	{"name": "A380 (Korean) [derived from Xavier's A380]", "id": 329, "url": "https://aircraft-loader.appspot.com/load.php?id=329"},
+	{"name": "A380 (British Airways) [derived from Xavier's A380]", "id": 330, "url": "https://aircraft-loader.appspot.com/load.php?id=330"},
+	{"name": "A380 (Dubai One) [Livery by Siddharth]", "id": 333, "url": "https://aircraft-loader.appspot.com/load.php?id=333"}
 ]
